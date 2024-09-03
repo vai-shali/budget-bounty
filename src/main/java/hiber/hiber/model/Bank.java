d60c67ff88264ec5560b6220a935c30b0c9b953b
Kiranmoy Saha <kiranmoy2002@gmail.com>
2024-09-03 22:23:08 +0530

    added comment model files

--- src/main/java/hiber/hiber/model/Bank.java
+++ src/main/java/hiber/hiber/model/Bank.java
@@ -14,37 +14,86 @@ import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
 /**
- * @author Vishal
- * @Since 2nd September, 2024
+ * Represents a bank entity with details such as bank ID, name, IFSC code,
+ * account number, UPI ID, account type, balance, and the associated user.
+ * This entity is mapped to the 'bank' table in the database.
+ *
+ * @author Inas
+ * @since 2nd September, 2024
  */
 @Entity
-@Table(name="bank")
+@Table(name = "bank")
 public class Bank {
-	@Id
-	@Column(name="bank_id")
+
+    /**
+     * Unique identifier for the bank.
+     */
+    @Id
+    @Column(name = "bank_id")
     private int bankId;
-    @Column(name="bank_name")
-	private String bankName;
+
+    /**
+     * Name of the bank.
+     */
+    @Column(name = "bank_name")
+    private String bankName;
+
+    /**
+     * IFSC code of the bank.
+     */
     private String ifsc;
+
+    /**
+     * Account number associated with the bank.
+     */
     @Column(name = "account_number")
     private String accountNumber;
+
+    /**
+     * UPI ID linked with the bank account.
+     */
     @Column(name = "upi_id")
     private String upiId;
+
+    /**
+     * Type of the bank account (e.g., savings, current).
+     */
     @Column(name = "account_type")
     private String accountType;
+
+    /**
+     * Balance available in the bank account.
+     */
     private double balance;
-//    @Column(name = "user_id")
-//    private int userId;
+
+    /**
+     * The user associated with the bank account.
+     */
     @OneToOne
     @PrimaryKeyJoinColumn(name = "user_id")
     private User user;
 
+    /**
+     * Default constructor for the Bank class.
+     */
     public Bank() {
-		super();
-	}
+        super();
+    }
 
-	// Constructor
-    public Bank(int bankId, String bankName, String ifsc, String accountNumber, String upiId, String accountType, double balance, User userObj) {
+    /**
+     * Constructor to initialize all fields of the Bank class.
+     *
+     * @param bankId        the unique identifier for the bank
+     * @param bankName      the name of the bank
+     * @param ifsc          the IFSC code of the bank
+     * @param accountNumber the account number associated with the bank
+     * @param upiId         the UPI ID linked with the bank account
+     * @param accountType   the type of the bank account (e.g., savings, current)
+     * @param balance       the balance available in the bank account
+     * @param userObj       the user associated with the bank account
+     */
+    public Bank(int bankId, String bankName, String ifsc, String accountNumber, String upiId, String accountType,
+                double balance, User userObj) {
         this.bankId = bankId;
         this.bankName = bankName;
         this.ifsc = ifsc;
@@ -52,87 +101,161 @@ public class Bank {
         this.upiId = upiId;
         this.accountType = accountType;
         this.balance = balance;
-        this.user=userObj;
+        this.user = userObj;
     }
 
-    // Getters and Setters
+    /**
+     * Gets the unique identifier for the bank.
+     *
+     * @return the bank ID
+     */
     public int getBankId() {
         return bankId;
     }
 
+    /**
+     * Sets the unique identifier for the bank.
+     *
+     * @param bankId the bank ID to set
+     */
     public void setBankId(int bankId) {
         this.bankId = bankId;
     }
 
+    /**
+     * Gets the name of the bank.
+     *
+     * @return the bank name
+     */
     public String getBankName() {
         return bankName;
     }
 
+    /**
+     * Sets the name of the bank.
+     *
+     * @param bankName the bank name to set
+     */
     public void setBankName(String bankName) {
         this.bankName = bankName;
     }
 
+    /**
+     * Gets the IFSC code of the bank.
+     *
+     * @return the IFSC code
+     */
     public String getIfsc() {
         return ifsc;
     }
 
+    /**
+     * Sets the IFSC code of the bank.
+     *
+     * @param ifsc the IFSC code to set
+     */
     public void setIfsc(String ifsc) {
         this.ifsc = ifsc;
     }
 
+    /**
+     * Gets the account number associated with the bank.
+     *
+     * @return the account number
+     */
     public String getAccountNumber() {
         return accountNumber;
     }
 
+    /**
+     * Sets the account number associated with the bank.
+     *
+     * @param accountNumber the account number to set
+     */
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
+    /**
+     * Gets the UPI ID linked with the bank account.
+     *
+     * @return the UPI ID
+     */
     public String getUpiId() {
         return upiId;
     }
 
+    /**
+     * Sets the UPI ID linked with the bank account.
+     *
+     * @param upiId the UPI ID to set
+     */
     public void setUpiId(String upiId) {
         this.upiId = upiId;
     }
 
+    /**
+     * Gets the type of the bank account.
+     *
+     * @return the account type
+     */
     public String getAccountType() {
         return accountType;
     }
 
+    /**
+     * Sets the type of the bank account.
+     *
+     * @param accountType the account type to set
+     */
     public void setAccountType(String accountType) {
         this.accountType = accountType;
     }
 
+    /**
+     * Gets the balance available in the bank account.
+     *
+     * @return the balance
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Sets the balance available in the bank account.
+     *
+     * @param balance the balance to set
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
-//    public int getUserId() {
-//        return userId;
-//    }
-//
-//    public void setUserId(int userId) {
-//        this.userId = userId;
-//    }
-    
-	@Override
-	public String toString() {
-		return "Bank [bankId=" + bankId + ", bankName=" + bankName + ", ifsc=" + ifsc + ", accountNumber="
-				+ accountNumber + ", upiId=" + upiId + ", accountType=" + accountType + ", balance=" + balance
-				;
-	}
+    /**
+     * Gets the user associated with the bank account.
+     *
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    /**
+     * Sets the user associated with the bank account.
+     *
+     * @param user the user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
-    
+    /**
+     * Returns a string representation of the Bank object.
+     *
+     * @return a string containing the bank details
+     */
+    @Override
+    public String toString() {
+        return "Bank [bankId=" + bankId + ", bankName=" + bankName + ", ifsc=" + ifsc + ", accountNumber="
+                + accountNumber + ", upiId=" + upiId + ", accountType=" + accountType + ", balance=" + balance + "]";
+    }
 }
