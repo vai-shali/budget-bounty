8194fcb6a720ce9022ba4bfd5e9d84d5c3028efd
Kiranmoy Saha <kiranmoy2002@gmail.com>
2024-09-03 22:37:29 +0530

    added comment to repository files

--- src/main/java/hiber/hiber/repository/BankRepository.java
+++ src/main/java/hiber/hiber/repository/BankRepository.java
@@ -11,81 +11,101 @@ import org.hibernate.query.Query;
 import hiber.hiber.model.Bank;
 import hiber.hiber.util.HibernateUtil;
 
+/**
+ * Repository class for managing Bank entities.
+ */
 public class BankRepository {
 
-    // Method to get all bank accounts
-    public List<Bank> getAllBanks() {
-    	List<Bank> banks = new ArrayList<>();
-        SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
+	/**
+	 * Retrieves all bank accounts from the database.
+	 *
+	 * @return a list of all Bank entities.
+	 */
+	public List<Bank> getAllBanks() {
+		List<Bank> banks = new ArrayList<>();
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
 		Query qr = s.createQuery("FROM Bank");
-		banks=qr.list();
-        return banks;
-    }
+		banks = qr.list();
+		return banks;
+	}
 
-    // Method to get a bank by bank ID
-    public Bank getBankById(int bankId) {
-    	SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
-		Query qr = s.createQuery("FROM Bank where bankId= :bankId");
+	/**
+	 * Retrieves a bank account by its ID.
+	 *
+	 * @param bankId the ID of the bank to retrieve.
+	 * @return the Bank entity with the specified ID, or null if not found.
+	 */
+	public Bank getBankById(int bankId) {
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
+		Query qr = s.createQuery("FROM Bank where bankId = :bankId");
 		qr.setParameter("bankId", bankId);
-		List<Bank> list=qr.list();
-		if(list.size()==0) {
+		List<Bank> list = qr.list();
+		if (list.size() == 0) {
 			return null;
 		}
 		return list.get(0);
-    }
+	}
 
-    // Method to add a new bank account
-    public void addBank(Bank bank) {
-    	SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
+	/**
+	 * Adds a new bank account to the database.
+	 *
+	 * @param bank the Bank entity to be added.
+	 */
+	public void addBank(Bank bank) {
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
 		try {
-			Transaction tran=s.beginTransaction();
+			Transaction tran = s.beginTransaction();
 			s.save(bank);
 			tran.commit();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-	        s.close();
-	    }
-    } 
+			s.close();
+		}
+	}
 
-    // Method to update an existing bank account
-    public void updateBank(Bank bank) {
-    	SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
-        try {
-    		Transaction tran=s.beginTransaction();
-    		s.update(bank);
-    		tran.commit();
-        }
-        catch(Exception e) {
-        	e.printStackTrace();
-        } finally {
-            s.close();
-        }
-    }
+	/**
+	 * Updates an existing bank account in the database.
+	 *
+	 * @param bank the Bank entity to be updated.
+	 */
+	public void updateBank(Bank bank) {
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
+		try {
+			Transaction tran = s.beginTransaction();
+			s.update(bank);
+			tran.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			s.close();
+		}
+	}
 
-    // Method to delete a bank account by ID
-    public void deleteBank(int bankId) {
-    	SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
-        try {
-    		Transaction tran=s.beginTransaction();
-    		String hql = "DELETE FROM Bank WHERE bankId = :bankId";
-    		Query qr = s.createQuery(hql);
-    		qr.setParameter("bankId", bankId);
+	/**
+	 * Deletes a bank account by its ID.
+	 *
+	 * @param bankId the ID of the bank to be deleted.
+	 */
+	public void deleteBank(int bankId) {
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
+		try {
+			Transaction tran = s.beginTransaction();
+			String hql = "DELETE FROM Bank WHERE bankId = :bankId";
+			Query qr = s.createQuery(hql);
+			qr.setParameter("bankId", bankId);
 
-    		qr.executeUpdate();
-    		tran.commit();
-        }
-        catch(Exception e) {
-        	e.printStackTrace();
-        } finally {
-            s.close();
-        }
-    }
+			qr.executeUpdate();
+			tran.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			s.close();
+		}
+	}
 }
-
--- src/main/java/hiber/hiber/repository/UserRepository.java
+++ src/main/java/hiber/hiber/repository/UserRepository.java
@@ -11,87 +11,101 @@ import org.hibernate.query.Query;
 import hiber.hiber.model.User;
 import hiber.hiber.util.HibernateUtil;
 
+/**
+ * Repository class for performing CRUD operations on {@link User} entities.
+ */
 public class UserRepository {
+
+	/**
+	 * Finds a user by their user ID.
+	 *
+	 * @param userId the ID of the user to find
+	 * @return the User entity if found, or null if no user with the given ID exists
+	 * @throws SQLException if there is an error accessing the database
+	 */
 	public User findById(int userId) throws SQLException {
-		SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
-		Query qr = s.createQuery("FROM User where userId= :userId");
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
+		Query qr = s.createQuery("FROM User where userId = :userId");
 		qr.setParameter("userId", userId);
-		List<User> list=qr.list();
-		if(list.size()==0) {
+		List<User> list = qr.list();
+		if (list.size() == 0) {
 			return null;
 		}
 		return list.get(0);
-    }
+	}
+
+	/**
+	 * Finds all users in the database.
+	 *
+	 * @return a List of all User entities, or null if no users exist
+	 * @throws SQLException if there is an error accessing the database
+	 */
 	public List<User> findAll() throws SQLException {
-		SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
 		Query qr = s.createQuery("FROM User");
-		List<User> list=qr.list();
-		if(list.size()==0) {
+		List<User> list = qr.list();
+		if (list.size() == 0) {
 			return null;
 		}
 		return list;
-    }
+	}
+
+	/**
+	 * Finds a user by their email address.
+	 *
+	 * @param email the email of the user to find
+	 * @return the User entity if found, or null if no user with the given email exists
+	 * @throws SQLException if there is an error accessing the database
+	 */
 	public User findByEmail(String email) throws SQLException {
-		SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
-		Query qr = s.createQuery("FROM User where email= :email");
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
+		Query qr = s.createQuery("FROM User where email = :email");
 		qr.setParameter("email", email);
-		List<User> list=qr.list();
-		if(list.size()==0) {
+		List<User> list = qr.list();
+		if (list.size() == 0) {
 			return null;
 		}
 		return list.get(0);
-    }
+	}
 
-    // Method to find a user by email and password
-    public User findByEmailAndPassword(String email, String password) throws SQLException {
-    	SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
-		Query qr = s.createQuery("FROM User where email= :email and password= :pass");
+	/**
+	 * Finds a user by their email address and password.
+	 *
+	 * @param email the email of the user to find
+	 * @param password the password of the user to find
+	 * @return the User entity if found, or null if no user with the given email and password exists
+	 * @throws SQLException if there is an error accessing the database
+	 */
+	public User findByEmailAndPassword(String email, String password) throws SQLException {
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
+		Query qr = s.createQuery("FROM User where email = :email and password = :pass");
 		qr.setParameter("email", email);
 		qr.setParameter("pass", password);
-		List<User> list=qr.list();
-		if(list.size()==0) {
+		List<User> list = qr.list();
+		if (list.size() == 0) {
 			return null;
 		}
 		return list.get(0);
-    }
-    
-    public void update(User user) throws SQLException {
-    	SessionFactory sf=HibernateUtil.getSessionFactoryObject();
- 		Session s=sf.openSession();
-    	 try {
-     		Transaction tran=s.beginTransaction();
-     		String hql = "UPDATE User SET name = :name, username = :username, password = :password, " +
-                     "phoneNumber = :phoneNumber, email = :email, role = :role " +
-                     "WHERE userId = :userId";
-     		Query qr = s.createQuery(hql);
-			qr.setParameter("userId", user.getUserId());
-			qr.setParameter("name", user.getName());
-			qr.setParameter("username", user.getUsername());
-			qr.setParameter("password", user.getPassword());
-			qr.setParameter("phoneNumber", user.getPhoneNumber());
-			qr.setParameter("email", user.getEmail());
-			qr.setParameter("role",user.getRole());
-			qr.executeUpdate();
-     		tran.commit();
-         }
-         catch(Exception e) {
-         	e.printStackTrace();
-         } finally {
-             s.close(); 
-         }
-    }
-    
-    public void save(User user) throws SQLException {
-    	SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
+	}
+
+	/**
+	 * Updates the details of an existing user.
+	 *
+	 * @param user the User entity with updated details
+	 * @throws SQLException if there is an error accessing the database
+	 */
+	public void update(User user) throws SQLException {
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
 		try {
-			Transaction tran=s.beginTransaction();
-			String hql = "INSERT INTO User (userId, name, username, password, phoneNumber, email, role) " +
-		             "SELECT :userId, :name, :username, :password, :phoneNumber, :email, :role";
+			Transaction tran = s.beginTransaction();
+			String hql = "UPDATE User SET name = :name, username = :username, password = :password, " +
+					"phoneNumber = :phoneNumber, email = :email, role = :role " +
+					"WHERE userId = :userId";
 			Query qr = s.createQuery(hql);
 			qr.setParameter("userId", user.getUserId());
 			qr.setParameter("name", user.getName());
@@ -99,34 +113,66 @@ public class UserRepository {
 			qr.setParameter("password", user.getPassword());
 			qr.setParameter("phoneNumber", user.getPhoneNumber());
 			qr.setParameter("email", user.getEmail());
-			qr.setParameter("role",user.getRole());
+			qr.setParameter("role", user.getRole());
 			qr.executeUpdate();
 			tran.commit();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-            s.close(); 
-        }
-    }
-    
-    public void delete(int userId) throws SQLException {
-    	SessionFactory sf=HibernateUtil.getSessionFactoryObject();
-		Session s=sf.openSession();
-    	try {
-    		Transaction tran=s.beginTransaction();
-    		String hql = "DELETE FROM User WHERE userId = :userId";
-    		Query qr = s.createQuery(hql);
-    		qr.setParameter("userId", userId);
+			s.close();
+		}
+	}
 
-    		qr.executeUpdate();
-    		tran.commit();
-        }
-        catch(Exception e) {
-        	e.printStackTrace();
-        } finally {
-            s.close(); 
-        }
-    }
+	/**
+	 * Saves a new user to the database.
+	 *
+	 * @param user the User entity to save
+	 * @throws SQLException if there is an error accessing the database
+	 */
+	public void save(User user) throws SQLException {
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
+		try {
+			Transaction tran = s.beginTransaction();
+			String hql = "INSERT INTO User (userId, name, username, password, phoneNumber, email, role) " +
+					"SELECT :userId, :name, :username, :password, :phoneNumber, :email, :role";
+			Query qr = s.createQuery(hql);
+			qr.setParameter("userId", user.getUserId());
+			qr.setParameter("name", user.getName());
+			qr.setParameter("username", user.getUsername());
+			qr.setParameter("password", user.getPassword());
+			qr.setParameter("phoneNumber", user.getPhoneNumber());
+			qr.setParameter("email", user.getEmail());
+			qr.setParameter("role", user.getRole());
+			qr.executeUpdate();
+			tran.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			s.close();
+		}
+	}
+
+	/**
+	 * Deletes a user by their user ID.
+	 *
+	 * @param userId the ID of the user to delete
+	 * @throws SQLException if there is an error accessing the database
+	 */
+	public void delete(int userId) throws SQLException {
+		SessionFactory sf = HibernateUtil.getSessionFactoryObject();
+		Session s = sf.openSession();
+		try {
+			Transaction tran = s.beginTransaction();
+			String hql = "DELETE FROM User WHERE userId = :userId";
+			Query qr = s.createQuery(hql);
+			qr.setParameter("userId", userId);
+			qr.executeUpdate();
+			tran.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			s.close();
+		}
+	}
 }
-
